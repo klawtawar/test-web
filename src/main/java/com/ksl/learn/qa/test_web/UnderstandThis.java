42f16ee2db16c6d87b4abcb1c38db5c84e5f677c
kunal <klawtawar@gmail.com>
2021-08-20 15:25:31 -0500

    updates

---
+++ src/main/java/com/ksl/learn/qa/test_web/UnderstandThis.java
@@ -0,0 +1,40 @@
+package com.ksl.learn.qa.test_web;
+
+
+
+class Test {
+	int pid = 10;
+	String comp = "IBM";
+	
+	Test(){
+		System.out.println("Test Construtor");
+	}
+	
+	public void display(int p, String c) {
+	
+		this.pid = p;
+		this.comp = c;
+		this.display();
+
+	}
+	
+	public void display() {
+		
+		System.out.println("display test method " + "pid" + pid + "comp" + comp); 
+
+	}
+
+	
+}
+public class UnderstandThis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Test t = new Test();
+		//	t.display();
+			t.display(222, "HCL");
+			
+	}
+
+}
