04b04f8dbf7eddc5eb1cda2a383b0df95d88ce24
kunal <klawtawar@gmail.com>
2021-09-01 14:59:56 -0500

    updates

--- src/main/java/com/ksl/learn/qa/test_web/App.java
+++ src/main/java/com/ksl/learn/qa/test_web/App.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -22,17 +23,17 @@ public class App
 {
     public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!1" );
+
         
         int[] list = {2,4,2,5,6,5,1,4};
         
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        Map<String, Integer> hm = new HashMap<String, Integer>();
 		/*
 		 * Map { "California" , 4 "Arkansas", 2 "texas",1 "florida", 2 "kansas", 1
 		 * "missouri", 4 }
 		 */
                
-        hm.put("California" , 4);
+        hm.put("California", 4);
         hm.put("Arkansas", 2);
         hm.put("Texas", 1);
         hm.put("florida", 2);
@@ -40,8 +41,15 @@ public class App
 
                
    //      System.out.println(hm.get("California"));
+        
+
+        for (Map.Entry<String, Integer> set : hm.entrySet()) {
+		//    System.out.println(set.getKey() + " = " + set.getValue());
+		    
+		    if(set.getValue() == 2) System.out.println(set.getKey());
+		}
          
-         System.out.println(hm.keySet());
+    //     System.out.println(hm.keySet());
          
          Set<String> keySet = hm.keySet();
          
@@ -52,12 +60,12 @@ public class App
          for(int i=0; i<listOfKeys.size(); i++){
         	 
         	 String str = listOfKeys.get(i);
-   //     	 System.out.println(str);
+      //  	 System.out.println(str);
     //    	 System.out.println(hm.get(str));
         	 
         	 if(hm.get(str) == 1) {
         		 
-        		 System.out.println(str);
+      //  		 System.out.println(str + " " + i);
       		 
         	 }      	 
          }
